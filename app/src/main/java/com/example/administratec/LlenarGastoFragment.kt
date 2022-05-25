6056a9e89ad0635455ad1b535e081a4df84af0f8
Eduardo Pineda Guerra <37886408+NeoMty@users.noreply.github.com>
2022-05-24 22:58:44 -0500

    Diseño de llenar gasto

--- app/src/main/java/com/example/administratec/LlenarGastoFragment.kt
+++ app/src/main/java/com/example/administratec/LlenarGastoFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import com.example.administratec.databinding.FragmentAgregarGastoBinding
+import com.example.administratec.databinding.FragmentLlenarGastoBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,17 +16,16 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [AgregarGastoFragment.newInstance] factory method to
+ * Use the [LlenarGastoFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AgregarGastoFragment : Fragment() {
+class LlenarGastoFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
-    private var _binding : FragmentAgregarGastoBinding? = null
+    private var _binding : FragmentLlenarGastoBinding? = null
     private val binding get() = _binding!!
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,7 +40,7 @@ class AgregarGastoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         //  Binding para Fragmentos
-        _binding = FragmentAgregarGastoBinding.inflate(inflater, container, false)
+        _binding = FragmentLlenarGastoBinding.inflate(inflater, container, false)
         val view = binding.root
 
         return view
@@ -48,24 +48,8 @@ class AgregarGastoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.buttonCompras.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_agregarGastoFragment_to_llenarGastoFragment)
-        }
-
-        binding.buttonCasa.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_agregarGastoFragment_to_llenarGastoFragment)
-        }
-
-        binding.buttonElectronicos.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_agregarGastoFragment_to_llenarGastoFragment)
-        }
-
-        binding.buttonAlimentacion.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_agregarGastoFragment_to_llenarGastoFragment)
-        }
-
-        binding.buttonEducacion.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_agregarGastoFragment_to_llenarGastoFragment)
+        binding.buttonGuardar.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_llenarGastoFragment_to_agregarGastoFragment)
         }
     }
 
@@ -76,12 +60,12 @@ class AgregarGastoFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment AgregarGastoFragment.
+         * @return A new instance of fragment LlenarGastoFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            AgregarGastoFragment().apply {
+            LlenarGastoFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
