69c409e8b07e0eec7fdc3431731014146944bbb0
Eduardo Pineda Guerra <37886408+NeoMty@users.noreply.github.com>
2022-06-14 17:10:31 -0500

    Date format fix 2

--- app/src/main/java/com/example/administratec/Adapter.kt
+++ app/src/main/java/com/example/administratec/Adapter.kt
@@ -21,7 +21,7 @@ class Adapter(var gasto: List<Gasto>): RecyclerView.Adapter<Adapter.ViewHolder>(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.binding.apply {
             val fecha = gasto[position].fecha
-            val spf = SimpleDateFormat("dd MM yyyy")
+            val spf = SimpleDateFormat("dd-MM-yyyy")
             val fecha_good = spf.format(fecha)
             textViewFecha.text = fecha_good
             when (gasto[position].categoria) {
