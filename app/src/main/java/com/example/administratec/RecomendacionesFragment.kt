4e5a404812ae7a84e1911920228962228cc1d071
Citlalli27 <70440578+Citlalli27@users.noreply.github.com>
2022-06-15 15:11:33 -0500

    Agregar recomendaciones con imagenes

--- app/src/main/java/com/example/administratec/RecomendacionesFragment.kt
+++ app/src/main/java/com/example/administratec/RecomendacionesFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.administratec.databinding.FragmentAgregarGastoBinding
+import com.example.administratec.databinding.FragmentRecomendacionesBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,13 +18,19 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [RecomendacionesFragment.newInstance] factory method to
+ * Use the [AgregarGastoFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
 class RecomendacionesFragment : Fragment() {
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private var _binding : FragmentRecomendacionesBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +44,16 @@ class RecomendacionesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recomendaciones, container, false)
+        //  Binding para Fragmentos
+        _binding = FragmentRecomendacionesBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
     companion object {
@@ -44,16 +63,17 @@ class RecomendacionesFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment RecomendacionesFragment.
+         * @return A new instance of fragment AgregarGastoFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            RecomendacionesFragment().apply {
+            AgregarGastoFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
     }
+
 }
